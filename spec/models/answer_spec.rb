6eee6fd73c0e13c4beaeb3197a9a5fc40194c1cb
KnighT178 <egor.skandakov@gmail.com>
2022-02-02 01:44:03 +0300

    Added tests for questions and answers

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  describe 'Associations' do
+    it { should belong_to(:question) }
+  end
 
+  describe 'Validations' do
+    it { should validate_presence_of :body }
+  end
 end
