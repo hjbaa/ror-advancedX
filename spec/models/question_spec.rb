f50c2730bfebae1f0cd6100b49bef66ac4dae173
KnighT178 <egor.skandakov@gmail.com>
2022-02-02 14:09:11 +0300

    Added test for dependent destroy & rubocop

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   describe 'Associations' do
     it { should have_many(:answers) }
+    it { should have_many(:answers).dependent(:destroy) }
   end
 
   describe 'Validations' do
