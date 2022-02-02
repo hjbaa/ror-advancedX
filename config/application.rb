8fbd631a57aff11efb940d2906b185d6f788fb55
KnighT178 <egor.skandakov@gmail.com>
2022-02-02 21:28:50 +0300

    Added questions controller & tests for it

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,11 @@ module RorAdvancedx
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec, view_specs: false,
+                               helper_specs: false,
+                               routing_specs: false,
+                               request_specs: false
+    end
   end
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -2,5 +2,6 @@
 
 FactoryBot.define do
   factory :answer do
+    body { 'MyString' }
   end
 end
