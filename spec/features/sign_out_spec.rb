d2f2e3c2e3e8a22cf70d99c4a0bfa2229beaa0c9
KnighT178 <egor.skandakov@gmail.com>
2022-02-09 09:49:20 +0300

    Added bootstrap, added index view, application.html.erb changed to application.html.slim, added acceptance test for log out, added header

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'User can sign out' do
+  given(:user) { User.create!(email: 'test@test.com', password: '12345678') }
+
+  background do
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+  end
+
+  scenario 'Current user tries to sign out' do
+    click_on 'Log out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
