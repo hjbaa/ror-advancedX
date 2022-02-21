b1ddaa8debb2098781418b0ccd08c8dbcc44b25c
KnighT178 <egor.skandakov@gmail.com>
2022-02-21 16:29:50 +0300

    attached files are deleted along with the question

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,7 +10,15 @@ class Question < ApplicationRecord
 
   validates :body, :title, presence: true
 
+  before_destroy :destroy_attached_files
+
   def answers_without_best
     answers.where.not(id: best_answer_id)
+  end
+
+  private
+
+  def destroy_attached_files
+    files.each(&:purge)
   end
 end
