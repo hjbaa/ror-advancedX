7f093e54d53284bfaf2c81599dc974962010f6d1
KnighT178 <egor.skandakov@gmail.com>
2022-02-28 19:59:54 +0300

    Validation for url format

--- spec/features/question/add_link_spec.rb
+++ spec/features/question/add_link_spec.rb
@@ -3,13 +3,16 @@ require 'rails_helper'
 feature 'User can add links to question', js: true do
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/hjbaa/d95569f97548d91d00219db59862a463' }
+  given(:google_url) { 'https://google.com' }
 
-  scenario 'User adds link when asks question' do
+  background do
     sign_in(user)
     visit new_question_path
+  end
+
+  scenario 'User adds link when asks question' do
     fill_in 'Your question', with: 'Question title'
     fill_in 'Description', with: 'text text text'
-
     fill_in 'Link name', with: 'My gist'
     fill_in 'Url', with: gist_url
 
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  it { should belong_to :linkable }
+  describe 'Associations' do
+    it { should belong_to :linkable }
+  end
 
-  it { should validate_presence_of :name }
-  it { should validate_presence_of :url }
+  describe 'Validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :url }
+
+    describe 'URL format' do
+      let!(:question) { create(:question) }
+
+      it 'should not be valid link' do
+        expect(Link.new(name: 'Some name', url: 'hello', linkable: question).valid?).to be_falsey
+      end
+
+      it 'should be valid link' do
+        expect(Link.new(name: 'Some name', url: 'https://google.com', linkable: question).valid?).to be_truthy
+      end
+    end
+  end
 end
