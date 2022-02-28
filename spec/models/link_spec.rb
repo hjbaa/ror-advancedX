d895a492ed03501a3964f2ff072fbff5cb141fb3
KnighT178 <egor.skandakov@gmail.com>
2022-02-28 22:41:26 +0300

    Refactored question & answer views

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -13,11 +13,27 @@ RSpec.describe Link, type: :model do
       let!(:question) { create(:question) }
 
       it 'should not be valid link' do
-        expect(Link.new(name: 'Some name', url: 'hello', linkable: question).valid?).to be_falsey
+        expect(Link.new(name: 'Some name', url: 'hello', linkable: question)).to_not be_valid
       end
 
       it 'should be valid link' do
-        expect(Link.new(name: 'Some name', url: 'https://google.com', linkable: question).valid?).to be_truthy
+        expect(Link.new(name: 'Some name', url: 'https://google.com', linkable: question)).to be_valid
+      end
+    end
+  end
+
+  describe 'Methods' do
+    describe 'gist?' do
+      let!(:question) { create(:question) }
+      let(:google_url) { 'https://google.com' }
+      let(:gist_url) { 'https://gist.github.com/hjbaa/d95569f97548d91d00219db59862a463' }
+
+      it 'should not be a gist' do
+        expect(Link.new(name: 'Some name', url: google_url, linkable: question)).to_not be_gist
+      end
+
+      it 'should not be a gist' do
+        expect(Link.new(name: 'Some name', url: gist_url, linkable: question)).to be_gist
       end
     end
   end
